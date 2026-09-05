17b22fa679f28c116e7bd75e4032a4de7deeab31
shivaranidara <darashivarani64@gmail.com>
2026-09-05 23:57:55 +0530

    AI debugging fix

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 
 def add_numbers(a, b):
-    return a+b
+    if isinstance(a, str) or isinstance(b, str):
+        return int(a) + int(b)
+    else:
+        return a + b
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     add_numbers(2,9)
