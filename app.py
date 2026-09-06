6c108a6b165a0b5e94ccd10260404f4ff976c773
shivaranidara <darashivarani64@gmail.com>
2026-09-06 16:06:36 +0530

    AI debugging checkpoint

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 
 def add_numbers(a, b):
-    if isinstance(a, str) or isinstance(b, str):
-        return int(a) + int(b)
-    else:
-        return a + b
+    return a+b
+        
 
 if __name__ == '__main__':
     add_numbers(2,9)
