39f38675eaaf68961678fcda1a57943d08cd9ad8
shivaranidara <darashivarani64@gmail.com>
2026-09-06 16:39:36 +0530

    AI debugging fix

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 
 def add_numbers(a, b):
-    return int(a) + b
+    try:
+        return int(a) + int(b)
+    except ValueError:
+        raise TypeError('Both arguments must be numbers')
         
 
 if __name__ == '__main__':
